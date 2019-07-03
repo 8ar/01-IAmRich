48307f884e3e445e31d895bfeb7c17bb8fe02f61
Carlos <crls8aslns@outlook.com>
2019-07-03 13:14:14 -0500

    Initial Commit

---
+++ 01-IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-IAmRich
+//
+//  Created by Adriana Ortega on 7/3/19.
+//  Copyright © 2019 Adriana Ortega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
